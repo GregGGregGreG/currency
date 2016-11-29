e2337690d32c5f043f95008ffbf0c27dff7ad0f4
Ilya Potapchuk <potapchuk.ilya@gmail.com>
2016-11-29 10:17:21 +0200

    update schema and add polyglot extension

--- schema-gen/src/main/resources/initial-data.sql
+++ schema-gen/src/main/resources/initial-data.sql
@@ -10,6 +10,6 @@ INSERT INTO role (id, role_name) VALUES
 INSERT INTO user (id, username, password) VALUES (11, 'admin', ?);
 INSERT INTO user_details (user_id, first_name, last_name, email) VALUES (11, 'Ilya', 'Potapchuk', 'ilya_potapchuk@mail.ru');
 INSERT INTO user_role (user_id, role_id) VALUES (11, 1002);
-INSERT INTO user_preferences (user_id, ui_notifications, mail_notifications, theme_name)  VALUES (11, 1, 0, 'valo-facebook');
+INSERT INTO user_preferences (user_id, ui_notifications, mail_notifications, theme_name, two_factor_auth, email_sign_in)  VALUES (11, 1, 0, 'valo-facebook', 0, 1);
 
 COMMIT;
